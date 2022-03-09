4483328c41ca9a12d97c5e7a4b3b3ec547505ff4
marocks31 <mollyarisley@gmail.com>
2022-03-08 20:50:19 -0600

    added two products

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,7 @@ Product.create(name: "Silly Putty", price: 5.99, image_url: "nothing for now", d
 Product.create(name: "Yo-yo", price: 9.99, image_url: "nothing to see here", description: "Yo-yo master certified, bright yellow")
 
 Product.create(name: "Chicago puzzle", price: 18.99, image_url: "nothing right now...", description: "5000 pieces, beware!")
+
+Product.create(name: "Silly string", price: 3.99, image_url: "you know", description: "very messy, orange")
+
+Product.create(name: "Rainbow Barbie", price: 14.99, image_url: "doesn't exist yet", description: "a skipper barbie with rainbow hair")
