9aee61c31cb8bf2dd62dbf00ab9761ee4185e93c
marocks31 <mollyarisley@gmail.com>
2022-04-20 20:18:05 -0500

    updated netlify url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "https://cerulean-nasturtium-e028f8.netlify.app"
+    origins "localhost:8080", "https://steady-kitsune-365395.netlify.app/products"
 
     resource "*",
       headers: :any,
